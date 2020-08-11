9475845cc1c63e4b7e3a44bc1c46f614d138a7b0
丁晓东 <dingxiaodongaaa@163.com>
2020-08-11 15:07:16 +0800

    init

---
+++ src/store/getters.js
@@ -0,0 +1,11 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  visitedViews: state => state.tagsView.visitedViews,
+  cachedViews: state => state.tagsView.cachedViews,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  permission_routes: state => state.permission.routes,
+  name: state => state.user.name
+}
+export default getters
